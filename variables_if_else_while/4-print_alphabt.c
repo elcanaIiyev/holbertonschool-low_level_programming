5535e6f11acc4d58c418c8c7c9be8fb87feb8f08
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-01 16:26:59 +0000

    elo

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point of all c programs
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	char letter = 'a';
+	while (letter <= 'z')
+	{	if (letter != 'e' && letter != 'q')
+			putchar(letter++);
+	}
+	putchar('\n');
+
+	return (0);
+{
