c81d8b78890d851554139edff9d424942692a1ea
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-01 15:51:10 +0000

    elo

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -9,11 +9,13 @@
  */
 int main(void)
 {
-	int n, last_digit;
+	int n,last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	last_digit = n % 10;
+
 	printf("Last digit of %d is %d \n", n, last_digit);
 	if (last_digit > 0)
 		printf(" and is greater than 5");
@@ -21,5 +23,6 @@ int main(void)
 		printf("and is 0");
 	else
 		printf("and is less than 6 and not 0);
+
 	return (0);
 }
