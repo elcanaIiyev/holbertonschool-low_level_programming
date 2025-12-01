9b88e98a1be95901173df4f895fd2ee9b4225104
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-01 16:11:31 +0000

    elo

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+/**
+ * main - entry point of all c programs
+ *
+ * Return: return 0 if the program is successful
+ */
+{
+	char letter = 'a', bletter = 'A'; 
+	while (letter <= 'z')
+		putchar(letter++);
+	while (bletter <= 'Z')
+		putchar(bletter++);
+	putchar('\n');
+	return (0);
+}
