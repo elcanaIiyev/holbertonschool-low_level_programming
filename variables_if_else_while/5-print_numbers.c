33b54b32b84ae975b74291ba87b961faa1e3a091
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-01 16:35:05 +0000

    elo

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point of all c programs
+ *
+ * Return: 0 if successfull
+ */
+int main(void)
+{
+	int a = 0;
+	while (a <= 9)
+		printf(a);
+	return (0)
+}
