ab418a054d4bcffe211ffd5dab090315a3f0bb6d
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-05 11:17:31 +0000

    elcan

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: 0 if successful
+ */
+
+void print_alphabet(void);
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}	
+	_putchar('\n');
+
+	return (0);
+}
