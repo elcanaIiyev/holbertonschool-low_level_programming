0608e2009253b8d8784437c161eb383b60ada084
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-09 13:40:35 +0000

    skill issue

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_array - entry point
+ * @a: parameter a
+ * @n: parameter n
+ */
+
+void print_array(int *a, int n)
+{
+
+	int i = 0;
+	
+	while (a[i] < a[n])
+	{
+		if (i != n - 1)
+			printf("%d, ", a[i]);
+	printf("%d", a[n-1]);
+	_putchar('\n');
+	
+}
