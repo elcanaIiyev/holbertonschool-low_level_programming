a7221a166d6d17f84952b4d106099881dc7cc4f7
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-09 10:50:01 +0000

    skill issue

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - entry point
+ * @s: parameter
+ */
+
+void rev_string(char *s)
+{
+	int i = 0, int j = 0;
+	char temp;
+
+	while (s[j] != '\0')
+		j++;
+	j--;
+	while (i < j)
+	{
+		temp = s[i];
+		s[i] = s[j];
+		s[j] = temp;
+		i++;
+		j--;
+}
