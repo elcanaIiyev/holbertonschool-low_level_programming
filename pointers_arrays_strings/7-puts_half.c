2820eafd771f76aa9b1b5a7da47c6a646c4ebfe6
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-09 12:55:22 +0000

    skill issue

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half - entry point
+ * @str: parameter
+ */
+
+void puts_half(char *str)
+{
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+		i++;
+	j = i / 2;
+
+	while (j < i)
+	{
+		_putchar(str[j]);
+		j++
+	}
+	_putchar('\n')
+}
