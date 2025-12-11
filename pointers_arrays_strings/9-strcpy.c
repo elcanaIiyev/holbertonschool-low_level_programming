5f0291d908d416c625132a84188609ed8d7437fa
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-11 12:33:24 +0000

    elo

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcpy - entry point
+ * @dest: parameter
+ * @src: parameter
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len = 0; i = 0;
+	
+	while (dest[len] != '\0')
+		len++;
+	while (src[i] != '\0')
+	{
+		dest[len] = src[i];
+		len++;
+		i++;
+	}
+	
+}
