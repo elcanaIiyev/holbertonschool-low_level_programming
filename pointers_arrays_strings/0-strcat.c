465c2d16a53b7dc248ffa6230dfc5400846e975d
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-12 16:10:55 +0000

    elcan

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - entry point
+ * @dest: parameter dest 
+ * @src: parameter src
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0; j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+	while ((dest[i] = src[j]))
+	{
+		i++;
+		j++;
+	}
+	return (dest);
+}
