081e0005f602bc3668044dfe2075f1e179533f6c
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-14 14:36:04 +0000

    elcan pointer day 2

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - entry point
+ * @str: string
+ * Return: str if successful
+ */
+
+char *string_toupper(char *str);
+{
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+		i++;
+	while (j < i)
+	{
+		if (str[j] <= 'a' && str[j] >= 'z')
+		{
+			str[j] -= 32;
+			j++;
+		}
+	}
+	return (str);
+}
