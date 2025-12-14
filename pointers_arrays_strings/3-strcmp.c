f2d7aed577de05cff8248605195695b067464bab
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-14 13:56:41 +0000

    elcan pointer day 2

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -16,8 +16,10 @@ int _strcmp(char *s1, char *s2)
 		if (s1[i] == s2[i])
 			n = (s1[i] - '0') - (s2[i] - '0');
 		else 
+		{
 			n = (s1[i] - '0') - (s2[i] - '0');
 			return(n);
+		}
 		i++;
 	}
 	return (n);
