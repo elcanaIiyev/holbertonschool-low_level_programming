ff73297850bc613a7104d8b003d204bfce959b49
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-14 14:11:39 +0000

    elcan pointer day 2

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: array
+ * @n: parameter
+ */
+
+void reverse_array(int *a, int n);
+{
+	int i = 0, temp = 0;
+
+	while (a[i] != a[n-1])
+	{
+		temp = a[i];
+		a[i] = a[n-1];
+		a[n-1] = temp;
+
+		i++;
+		n--;
+	}
+}
