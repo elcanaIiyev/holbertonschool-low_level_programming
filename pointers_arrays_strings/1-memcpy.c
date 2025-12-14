250701a71d001857dc15ea02562783764105bf1b
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-14 18:07:58 +0000

    elcan

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * _memcpy - entry point
+ * @dest: parameter
+ * @src: source
+ * @n: number of elements
+ * Return: returns destination if successful
+ */
+
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
