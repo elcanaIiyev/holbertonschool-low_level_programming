04ad9ba00b6f79cc0e294aa06d3f643fe421d068
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-14 18:17:45 +0000

    elcan

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - entry point
+ * @s: string
+ * @c: parameter c
+ * Return: returns element if successful
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] = c)
+			return (s[i]);
+		i++;
+	}
+	return('\0');
+}
