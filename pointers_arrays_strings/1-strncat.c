1a21c0ae7c2370d30ed53bbdec921b763cc2630a
elcanaIiyev <elcanaliyevinfo@gmail.com>
2025-12-14 13:29:21 +0000

    elcan pointer day 2

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncat - entry point
+ *
+ * @dest: parameter dest
+ *
+ * @src: parameter src
+ *
+ * @n: parameter n
+ *
+ * Return: dest if successful
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j, k = 0;
+
+	if (*(dest) == '\0' || *(src) == '\0')
+		return (dest);
+	while (dest[i] != '\0')
+		i++;
+	j = i;
+	while (j < i + i)
+	{
+		dest[i] = src[k];
+		j++;
+		k++;
+	}
+}
